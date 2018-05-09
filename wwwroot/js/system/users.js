8d15f9fd44a028969218c92d0505c854a05aae42
S-Nine <caiyong1024@outlook.com>
2018-05-09 19:06:05 +0800

    add user

--- wwwroot/js/system/users.js
+++ wwwroot/js/system/users.js
@@ -1,5 +1,5 @@
 $(function() {
-    // initEvent();
+    initEvent();
     initDatagrid();
 });
 
@@ -94,4 +94,76 @@ function initButtonEvent() {
     $('#btn_search') != null && $('#btn_search').click(function() {
         $('#users_datagrid').datagrid('reload');
     });
+
+    $('#btn_add') != null && $('#btn_add').click(function() {
+        cleanAddUserDialog();
+        $('#dlg_add_user').dialog('open');
+    });
+}
+
+/**
+ * 初始化事件
+ */
+function initEvent(){
+    $('#dlg_add_user').dialog({
+        toolbar: [{
+            id: 'user_save',
+            text: '保存',
+            iconCls: 'icon-add',
+            handler: function() {
+                if (isValidate('dlg_add_user')) {
+                    if ($('#user_save').linkbutton("options").disabled) { // 防止重复提交数据
+                        return;
+                    }
+                    $('#user_save').linkbutton("disable"); // 禁用
+
+                    var params = {
+                        
+                    };
+                    $.ajax({
+                        url: params.id == null || params.id == '' ? '/System/AddUser/' : '/System/UpdateUser/',
+                        type: 'post',
+                        dataType: 'json',
+                        data: params,
+                        success: function(data) {
+                            if ($('#user_save').linkbutton("options").disabled) { // 防止重复提交数据
+                                $('#user_save').linkbutton("enable"); // 启用
+                            }
+                            if (data != null && data.code == 200) {
+                                $('#dlg_add_user').dialog('close');
+                                $.messager.alert('提示', '保存成功！');
+                                $('#users_datagrid').datagrid('reload');
+                            } else {
+                                $.messager.alert('提示', '保存失败！');
+                            }
+                        },
+                        error: function() {
+                            $.messager.alert('错误', '网络连接失败、请稍后再试！');
+                            if ($('#user_save').linkbutton("options").disabled) { // 防止重复提交数据
+                                $('#user_save').linkbutton("enable"); // 启用
+                            }
+                        }
+                    });
+                }
+            }
+        }]
+    });
+}
+
+/**
+ * 清空用户Dialog
+ */
+function cleanAddUserDialog() {
+    if ($('#user_save').linkbutton("options").disabled) {
+        $('#user_save').linkbutton("enable");
+    }
+
+    $('#hid_id').val('');
+    $('#txt_name').textbox('clear');
+    $('#txt_login_name').textbox('clear');
+    $('#txt_login_pwd').passwordbox('clear');
+    $('#txt_tel').textbox('clear');
+    $('#txt_email').textbox('clear');
+    $('#rad_gender1').prop('checked','checked');
+    $('#rad_state1').prop('checked','checked');
 }
